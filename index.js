095b509b6701250fe5a9e27fc0de669d911f912e
Ndiema-S <you@example.com>
2022-08-04 23:28:39 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,70 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+
+}
+
+function subtract(a, b) {
+
+    return (a - b)
+
+}
+
+function multiply(a, b) {
+
+    return (a * b)
+}
+
+function divide(a, b) {
+
+    return (a / b)
+}
+
+
+function add(a, b) {
+
+    return (a + b)
+}
+function increment(n) {
+    return (++n)
+}
+
+function decrement(n) {
+
+    return (--n)
+
+}
+
+function makeInt(n) {
+
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
+//describe('makeInt(n)', function() {
+    //it('parses n as an integer and returns the parsed integer', function() {
+      //  expect(makeInt(a.toString())).toEqual(a)
+     // })
+
+
+//function add(a, b) {
+    //return (a + b)
+//}
+//console.log(add(5, 6));
